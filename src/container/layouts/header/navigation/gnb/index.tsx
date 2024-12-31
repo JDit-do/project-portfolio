f89eb54c6b34704a00b30cad8bac19275d75a17d
JDit-do <justdo.ju@gmail.com>
2024-12-31 21:49:29 +0900

    feat: gnb 파일 이전 commit과 연결된 파일

--- src/container/layouts/header/navigation/gnb/index.tsx
+++ src/container/layouts/header/navigation/gnb/index.tsx
@@ -1,13 +1,16 @@
+import Link from 'next/link';
+
 import { getTranslations } from 'next-intl/server';
 
+import { getPath } from '@/utils/header';
 import { APIResponse, GnbItem } from '@/types/api';
 
 import { APIEndpoints } from '@/constants/apiEndPoint';
 
 import style from './index.module.scss';
-import Link from 'next/link';
 
 export default async function GNB() {
+  const path = await getPath();
   const t = await getTranslations('navigation');
 
   const response = await fetch(APIEndpoints.navigation.gnb.url);
@@ -16,7 +19,10 @@ export default async function GNB() {
   return (
     <ul className={style.wrap}>
       {data.map(({ id, langKey, url }) => (
-        <li key={`gnb-${id}`}>
+        <li
+          key={`gnb-${id}`}
+          className={path === url ? style.active : undefined}
+        >
           <Link href={url}>{t(langKey)}</Link>
         </li>
       ))}
