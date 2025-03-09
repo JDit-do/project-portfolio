e0aac9b698aee8cd2563918654f4fcb130e5d566
JDit-do <justdo.ju@gmail.com>
2025-03-10 05:03:26 +0900

    feat: home 퍼블리싱

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,16 +1,11 @@
-import style from './page.module.scss';
-import HomeCanvas from '@/container/home/canvas';
 import HomeHero from '@/container/home/hero';
+
+import style from './page.module.scss';
 
 export default function HomePage() {
   return (
-    <div className={style.wrap}>
-      <div className={style.canvas}>
-        <HomeCanvas />
-      </div>
-      <div className={style.heroSection}>
-        <HomeHero />
-      </div>
-    </div>
+    <section className={style.wrap}>
+      <HomeHero />
+    </section>
   );
 }
