6b8e393de114bab696163408e86da036afd2d511
JDit-do <justdo.ju@gmail.com>
2025-03-10 04:21:51 +0900

    feat: API G.N.B key 변경 및 G.N.B 조건 추가
    - API G.N.B key 변경
    - G.N.B 1개일때 Hidden

--- src/container/layouts/header/navigation/gnb/index.tsx
+++ src/container/layouts/header/navigation/gnb/index.tsx
@@ -1,7 +1,8 @@
 import Link from 'next/link';
 
-import { getTranslations } from 'next-intl/server';
+import { getLocale } from 'next-intl/server';
 
+import { TLOCALE } from '@/types/common';
 import { getPath } from '@/utils/header';
 import { APIResponse, GnbItem } from '@/types/api';
 
@@ -11,21 +12,23 @@ import style from './index.module.scss';
 
 export default async function GNB() {
   const path = await getPath();
-  const t = await getTranslations('navigation');
+  const locale = await getLocale();
 
   const response = await fetch(APIEndpoints.navigation.gnb.url);
   const { data = [] }: APIResponse<GnbItem[]> = await response.json();
 
+  if (!(data.length > 1)) return <></>;
   return (
     <ul className={style.wrap}>
-      {data.map(({ id, langKey, url }) => (
-        <li
-          key={`gnb-${id}`}
-          className={path === url ? style.active : undefined}
-        >
-          <Link href={url}>{t(langKey)}</Link>
-        </li>
-      ))}
+      {data.map(({ id, url, menu }) => {
+        const _locale = locale as TLOCALE;
+
+        return (
+          <li key={id} className={path === url ? style.active : undefined}>
+            <Link href={url}>{menu[_locale]}</Link>
+          </li>
+        );
+      })}
     </ul>
   );
 }
